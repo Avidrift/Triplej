568f28db9d5cff50c3d0098683187e9b99530e51
Dragion9 <148816663+Dragion9@users.noreply.github.com>
2025-05-16 15:34:19 -0500

    Nuevas migraciones y modelos

--- database/migrations/2025_05_16_202702_create_admins_table.php
+++ database/migrations/2025_05_16_202702_create_admins_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('admins');
     }
 };
